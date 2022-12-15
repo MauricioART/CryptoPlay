40a85a49c025b36a11f4bf7002d397ee121ae6d6
David Contreras <davidco7890@gmail.com>
2022-12-15 14:02:43 -0600

    probando conexión

--- Frontend/d-app/src/pages/home/Home.tsx
+++ Frontend/d-app/src/pages/home/Home.tsx
@@ -1,5 +1,39 @@
+import { Button } from '@gear-js/ui';
+import { getWasmMetadata, GearApi } from '@gear-js/api';
+
+const gear = async function connect() {
+  const gearApi = await GearApi.create({
+    providerAddress: 'wss://rpc-node.gear-tech.io',
+  });
+
+  const [chain, nodeName, nodeVersion] = await Promise.all([
+    gearApi.chain(),
+    gearApi.nodeName(),
+    gearApi.nodeVersion(),
+  ]);
+
+  console.log(
+    `You are connected to chain ${chain} using ${nodeName} v${nodeVersion}`,
+  );
+
+  const unsub = await gearApi.gearEvents.subscribeToNewBlocks((header) => {
+    console.log(
+      `New block with number: ${header.number.toNumber()} and hash: ${header.hash.toHex()}`,
+    );
+  });
+
+  const message = {
+    destination: 'id',
+    payload: 'PING',
+    gasLimit: 10000000
+  };
+}
+
+
 function Home() {
-  return <div>Home page</div>;
+
+
+  return (<Button text="connect" onClick={gear}/>);
 }
 
 export { Home };
